71b230c37b250ffc97769b21fe5cdb329a7fbac2
Zach <zdelrosario@outlook.com>
2016-06-01 17:42:09 -0700

    fixed label code

--- final_project/code/get_basis.py
+++ final_project/code/get_basis.py
@@ -18,57 +18,33 @@ def get_basis(my_base,dim):
         Phi = [lambda x, i=i: x[i] for i in range(dim)] + \
               [lambda x, i=i: x[i]**2 for i in range(dim)] + \
               [lambda x, i=i: log(abs(x[i])) for i in range(dim)]
-        # Gradients 
-        dPhi = [gh(f)[0] for f in Phi]
+        # Labels
+        Labels = ["L_"+str(i) for i in range(dim)] + \
+                 ["Q_"+str(i) for i in range(dim)] + \
+                 ["G_"+str(i) for i in range(dim)]
         # Gradients 
         dPhi = [gh(f)[0] for f in Phi]
     elif my_base == 2:
         # Third Order
-        # Phi = [ lambda x: x[0],
-        #         lambda x: x[1],
-        #         lambda x: x[2],
-        #         lambda x: x[0]**2,
-        #         lambda x: x[1]**2,
-        #         lambda x: x[2]**2,
-        #         lambda x: x[0]**3,
-        #         lambda x: x[1]**3,
-        #         lambda x: x[2]**3,
-        #         lambda x: log(abs(x[0])),
-        #         lambda x: log(abs(x[1])),
-        #         lambda x: log(abs(x[2])),
-        #         lambda x: x[0]**(-1),
-        #         lambda x: x[1]**(-1),
-        #         lambda x: x[2]**(-1),]
         Phi = [lambda x, i=i: x[i] for i in range(dim)] + \
               [lambda x, i=i: x[i]**2 for i in range(dim)] + \
               [lambda x, i=i: x[i]**3 for i in range(dim)] + \
               [lambda x, i=i: log(abs(x[i])) for i in range(dim)] + \
               [lambda x, i=i: x[i]**(-1) for i in range(dim)]
+        # Labels
+        Labels = ["L_"+str(i) for i in range(dim)] + \
+                 ["Q_"+str(i) for i in range(dim)] + \
+                 ["C_"+str(i) for i in range(dim)] + \
+                 ["G_"+str(i) for i in range(dim)] + \
+                 ["I_"+str(i) for i in range(dim)]
+        # Gradients 
+        dPhi = [gh(f)[0] for f in Phi]
     else:
         # Active Subspace
-        Phi = []
-        Phi = Phi + [lambda x: x[i] for i in range(dim)]
+        Phi = [lambda x: x[i] for i in range(dim)]
+        # Labels
+        Labels = ["L_"+str(i) for i in range(dim)]
         # Gradients 
         dPhi = [gh(f)[0] for f in Phi]
 
-    Labels = [] # DEBUG
     return Phi, dPhi, Labels
-
-if __name__ == "__main__":
-    import numpy as np
-    m = 3
-
-    p1,d1,l1 = get_basis(1,m)
-    p2,d2,l2 = get_basis(1.5,m)
-
-    x = [1,2,3]
-
-    print( np.array(d1[0](x)) - np.array(d2[0](x)) )
-    print( np.array(d1[1](x)) - np.array(d2[1](x)) )
-    print( np.array(d1[2](x)) - np.array(d2[2](x)) )
-    print( np.array(d1[3](x)) - np.array(d2[3](x)) )
-    print( np.array(d1[4](x)) - np.array(d2[4](x)) )
-    print( np.array(d1[5](x)) - np.array(d2[5](x)) )
-    print( np.array(d1[6](x)) - np.array(d2[6](x)) )
-    print( np.array(d1[7](x)) - np.array(d2[7](x)) )
-    print( np.array(d1[8](x)) - np.array(d2[8](x)) )
